c021f1f1de7e568928f3275de01ee1b85d893867
Marco Stadler <marco-stadler@gmx.ch>
2023-05-08 18:39:49 +0200

    Update dependencies
    Readd service worker registration only in production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     BrowserModule,
     FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
